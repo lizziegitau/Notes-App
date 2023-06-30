33d2eee944d792b36e4448b1a2571305c6b08486
lizziegitau <lizziegitau0505@gmail.com>
2023-07-01 00:10:53 +0300

    final commit

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
 import Split from "react-split"
-import { nanoid } from "nanoid"
+
 import {
     onSnapshot,
     addDoc,
