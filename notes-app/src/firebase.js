e69613dfdcfafbc94131cfb26b019b5b2949b0a9
lizziegitau <lizziegitau0505@gmail.com>
2023-07-01 00:24:29 +0300

    db commit

--- notes-app/src/firebase.js
+++ notes-app/src/firebase.js
@@ -13,4 +13,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
 const notesCollection = collection(db, "notes")
-export { notesCollection };
+export { notesCollection , db};
